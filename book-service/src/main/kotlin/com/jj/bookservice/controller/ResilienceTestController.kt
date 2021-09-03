d7c91b80e8fa7138349d1b16c8f4349ccecda275
josimar-junior <juniorpaz99@gmail.com>
2021-09-03 00:23:07 -0300

    Using Rate Limit and BulkHead

--- book-service/src/main/kotlin/com/jj/bookservice/controller/ResilienceTestController.kt
+++ book-service/src/main/kotlin/com/jj/bookservice/controller/ResilienceTestController.kt
@@ -1,6 +1,8 @@
 package com.jj.bookservice.controller
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter
 import io.github.resilience4j.retry.annotation.Retry
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -17,12 +19,15 @@ class ResilienceTestController {
 
     @GetMapping("resilience")
     //@Retry(name = "resilience", fallbackMethod = "fallbackMethod")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    //@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    //@RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     fun resilience(): String? {
         logger.info("Request to resilience is received!")
-        val response = RestTemplate()
+        /*val response = RestTemplate()
             .getForEntity("http://localhost:8080/test", String::class.java)
-        return response.body
+        return response.body*/
+        return "OK!!!"
     }
 
     fun fallbackMethod(e: Exception) : String = "fallbackMethod : " + e.message
